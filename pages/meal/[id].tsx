1e8ce1b0893c2555f6f62a6b5ee820a63dcd84ab
Rocolusso <prolight@prolights-MacBook-Pro.local>
2025-03-31 17:01:34 +0300

    updated SinglePage component

--- pages/meal/[id].tsx
+++ pages/meal/[id].tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import {SingleMeals} from "./../../lib/types";
 import Image from "next/image";
+import {useRouter} from "next/router";
 
 const SingleReceipt = ({meals}:{meals:SingleMeals}) => {
+   const router = useRouter();
+
     return (
         <div className={'cotainer mx-auto flex justify-center items-center'}>
-            <div className={' mt-5 max-w-[800px] grid grid-cols-2 rounded-xl border border-gray-200 shadow-xl'}>
+            <div className={'relative mt-5 max-w-[800px] grid grid-cols-2 rounded-xl border border-gray-200 shadow-xl'}>
+                <div className={'absolute top-0 left-[-40px]'}>
+                    <button
+                        onClick={() => {router.push('/')}}
+                        type={'button'}
+                        className={'px-5 py-3 bg-black text-white rounded cursor-pointer'}
+                    >BACK</button>
+                </div>
                 <div >
                     <Image
                         className={'p-5 rounded-xl'}
@@ -31,10 +41,44 @@ const SingleReceipt = ({meals}:{meals:SingleMeals}) => {
                     <div>
                         <p>Tags: {meals.strTags}</p>
                     </div>
-                    <div >
+                    <div>
                         <p>Instructions: </p>
                         <div className={'p-5'}>
                             {meals.strInstructions}
+                        </div>
+                    </div>
+                    <div className={'ingredients'}>
+                        <div>
+                            <p>Ingredients</p>
+                        </div>
+                        <ul className={'p-5 grid grid-cols-2 rounded-xl border border-gray-200 '}>
+                            {meals.strIngredient1 && <li><div><p>{meals.strIngredient1}</p></div></li>}
+                            {meals.strIngredient2 && <li><div><p>{meals.strIngredient2}</p></div></li>}
+                            {meals.strIngredient3 && <li><div><p>{meals.strIngredient3}</p></div></li>}
+                            {meals.strIngredient4 && <li><div><p>{meals.strIngredient4}</p></div></li>}
+                            {meals.strIngredient5 && <li><div><p>{meals.strIngredient5}</p></div></li>}
+                            {meals.strIngredient6 && <li><div><p>{meals.strIngredient6}</p></div></li>}
+                            {meals.strIngredient7 && <li><div><p>{meals.strIngredient7}</p></div></li>}
+                            {meals.strIngredient8 && <li><div><p>{meals.strIngredient8}</p></div></li>}
+                            {meals.strIngredient9 && <li><div><p>{meals.strIngredient9}</p></div></li>}
+                            {meals.strIngredient10 && <li><div><p>{meals.strIngredient10}</p></div></li>}
+                            {meals.strIngredient11 && <li><div><p>{meals.strIngredient11}</p></div></li>}
+                            {meals.strIngredient12 && <li><div><p>{meals.strIngredient12}</p></div></li>}
+                            {meals.strIngredient13 && <li><div><p>{meals.strIngredient13}</p></div></li>}
+                            {meals.strIngredient14 && <li><div><p>{meals.strIngredient14}</p></div></li>}
+                            {meals.strIngredient15 && <li><div><p>{meals.strIngredient15}</p></div></li>}
+                            {meals.strIngredient16 && <li><div><p>{meals.strIngredient16}</p></div></li>}
+                            {meals.strIngredient17 && <li><div><p>{meals.strIngredient17}</p></div></li>}
+                            {meals.strIngredient18 && <li><div><p>{meals.strIngredient18}</p></div></li>}
+                            {meals.strIngredient19 && <li><div><p>{meals.strIngredient19}</p></div></li>}
+                            {meals.strIngredient20 && <li><div><p>{meals.strIngredient20}</p></div></li>}
+
+                        </ul>
+                    </div>
+                    <div className={'rounded-xl p-3 border border-gray-200'}>
+                        <div>
+                            <p>YouTube:</p>
+                            <p>{meals.strYoutube}</p>
                         </div>
                     </div>
                 </div>
